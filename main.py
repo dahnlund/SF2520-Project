115895447ec7ccdd9c6d7129bf0b8bbc3a90f369
Albert Sund Aillet <albert@sundaillet.com>
2023-12-06 20:19:55 +0100

    simplified shape definitions

--- main.py
+++ main.py
@@ -13,30 +13,29 @@ if get_ipython() is not None:  # Only run while in Jupyter
 
 # %%
 class DAEModule:
-    def __init__(self, A, A1, A2, dtau):
+    def __init__(self, A, A1, A2, dtau, M, N):
         self.A = A
         self.A1 = A1
         self.A2 = A2
         self.dtau = dtau
+        self.M = M
+        self.N = N
 
     def solve(self, epsilon: float = 0):
-        M = self.A1.shape[0] + 1
-        I_A1 = np.eye(self.A1.shape[0])
-        I_tot = np.zeros(self.A.shape)
+        I_A1 = np.eye(M - 1)
+        I_tot = np.zeros((M + N - 1, M + N - 1))
         I_tot[: M - 1, : M - 1] = I_A1
 
         if epsilon != 0:
-            I_tot[M - 1 :, M - 1 :] = 1 / epsilon * np.eye(self.A2.shape[0] + 1)
+            I_tot[M - 1 :, M - 1 :] = 1 / epsilon * np.eye(N)
             I_tot = csc_matrix(I_tot)
-            u0 = np.ones((self.A.shape[1], 1))
-            LHS = splu(
-                eye(self.A.shape[0], format="csc") - self.dtau * I_tot.dot(self.A)
-            )
+            u0 = np.ones((M + N - 1, 1))
+            LHS = splu(eye(M + N - 1, format="csc") - self.dtau * I_tot.dot(self.A))
             RHS = lambda uk: uk
 
         else:
             I_tot = csc_matrix(I_tot)
-            u0 = np.zeros((self.A.shape[1], 1))
+            u0 = np.zeros((M + N - 1, 1))
             u0[: M - 1] = 1
             LHS = splu(I_tot - self.dtau * self.A)
             RHS = lambda uk: I_tot.dot(uk)
@@ -72,9 +71,9 @@ N = int(round(w / dz))
 z = np.linspace(0, 1 + w, M + N + 1)
 A1_data = np.array(
     [
-        eta * np.ones(z[1:M].shape) / (v(z[1:M] + dz) * dz**2),
-        -2 * eta * np.ones(z[1:M].shape) / (v(z[1:M]) * dz**2),
-        eta * np.ones(z[1:M].shape) / (v(z[1:M] - dz) * dz**2),
+        eta * np.ones(M - 1) / (v(z[1:M] + dz) * dz**2),
+        -2 * eta * np.ones(M - 1) / (v(z[1:M]) * dz**2),
+        eta * np.ones(M - 1) / (v(z[1:M] - dz) * dz**2),
     ]
 )
 A1 = sp.spdiags(A1_data, np.array([-1, 0, 1]), format="csc")
@@ -85,39 +84,35 @@ A1[0, 1] = eta / (v(z[1]) * dz**2) * (2 / 3)
 
 A2_data = np.array(
     [
-        np.ones(z[M + 1 : M + N].shape) / (dz**2),
-        -2 * np.ones(z[M + 1 : M + N].shape) / (dz**2) - gamma,
-        np.ones(z[M + 1 : M + N].shape) / (dz**2),
+        np.ones(N - 1) / (dz**2),
+        -2 * np.ones(N - 1) / (dz**2) - gamma,
+        np.ones(N - 1) / (dz**2),
     ]
 )
 A2 = sp.spdiags(A2_data, np.array([-1, 0, 1]), format="csc")
 A2[-1, -1] = 1 / (dz**2) * (-2 / 3) - gamma
 A2[-1, -2] = 1 / (dz**2) * (2 / 3)
 
-b1 = np.zeros((A1.shape[0], 1))
+b1 = np.zeros((M - 1, 1))
 b1[-1] = -1 / dz
-b2 = np.zeros((A2.shape[0], 1))
+b2 = np.zeros((N - 1, 1))
 b2[0] = -alpha / dz
 a = (1 + alpha) / dz
-e1 = np.zeros((A1.shape[0], 1))
+e1 = np.zeros((M - 1, 1))
 e1[-1] = eta / ((dz**2) * v((M - 1) * dz))
 
-e2 = np.zeros((A2.shape[0], 1))
+e2 = np.zeros((N - 1, 1))
 e2[0] = 1 / (dz**2)
 
 # %% Concatenate everything to block matrix system
-block1 = sp.hstack(
-    [A1, csc_matrix(e1), csc_matrix(np.zeros((A1.shape[0], A2.shape[1])))]
-)
+block1 = sp.hstack([A1, csc_matrix(e1), csc_matrix(np.zeros((M - 1, N - 1)))])
 block2 = sp.hstack([csc_matrix(b1.T), csc_matrix(a), csc_matrix(b2.T)])
-block3 = sp.hstack(
-    [csc_matrix(np.zeros((A2.shape[0], A1.shape[1]))), csc_matrix(e2), A2]
-)
+block3 = sp.hstack([csc_matrix(np.zeros((N - 1, M - 1))), csc_matrix(e2), A2])
 A = sp.vstack([block1, block2, block3])
 
 # %%
 dtau = 0.001
-model = DAEModule(A, A1, A2, dtau)
+model = DAEModule(A, A1, A2, dtau, M, N)
 
 tau, u = model.solve()
 _, u_reg = model.solve(0.01)
@@ -131,7 +126,7 @@ ax = fig.add_subplot(111, projection="3d")
 ax.plot_surface(Z, TAU, u_reg.T)
 ax.plot_surface(Z, TAU, u.T)
 ax.legend(("u with reg", "u"))
-ax.set_zlim(0,1)
+ax.set_zlim(0, 1)
 plt.show()
 
 
@@ -142,12 +137,13 @@ fig, ax = plt.subplots()
 (line1,) = ax.plot(z[1:-1], u[:, 0], label="u")
 (line2,) = ax.plot(z[1:-1], u_reg[:, 0], label="u with reg")
 
+
 def update(frame):
     line1.set_ydata(u[:, frame])
     line2.set_ydata(u_reg[:, frame])
 
     ax.legend()
-    ax.set_ylim(0,1)
+    ax.set_ylim(0, 1)
     return (line1, line2)
 
 
